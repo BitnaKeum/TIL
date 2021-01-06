559e1dda6b834a34942eb123378118e4e6ff8736
BitnaKeum <beausty23@naver.com>
2021-01-06 18:18:36 +0900

    겨울SW학교 2일차

--- C/Day2_03.cpp
+++ C/Day2_03.cpp
@@ -1,23 +1,23 @@
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int arr[10] = { 0 };
-//
-//	for (int i = 0; i <= 9; i++) {
-//		printf("arr[%d] : ", i);
-//		scanf_s("%d", &arr[i]);
-//	}
-//
-//	int* pSm = arr;
-//	int* pWalk = arr + 1;
-//	int* pLast = arr + 9;
-//
-//	while (pWalk <= pLast) {
-//		if (*pWalk < *pSm)
-//			pSm = pWalk;
-//		pWalk++;
-//	}
-//
-//	printf("�ּҰ� : %d\n", *pSm);
-//}
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };
+
+	for (int i = 0; i <= 9; i++) {
+		printf("arr[%d] : ", i);
+		scanf_s("%d", &arr[i]);
+	}
+
+	int* pSm = arr;
+	int* pWalk = arr + 1;
+	int* pLast = arr + 9;
+
+	while (pWalk <= pLast) {
+		if (*pWalk < *pSm)
+			pSm = pWalk;
+		pWalk++;
+	}
+
+	printf("ÃÖ¼Ò°ª : %d\n", *pSm);
+}
