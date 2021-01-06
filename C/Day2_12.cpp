32786c30784e32465bdd74db202667850435a1ff
BitnaKeum <beausty23@naver.com>
2021-01-06 18:17:52 +0900

    겨울SW학교 2일차

--- C/Day2_12.cpp
+++ C/Day2_12.cpp
@@ -1,40 +1,40 @@
-//#include <stdio.h>
-//
-//int binarySearch(int A[], int first, int last, int key);
-//
-//int main()
-//{
-//	int arr[10] = { 0 };
-//	int search = 0;
-//	int key_idx = 0;
-//
-//	printf("¿ø½Ã µ¥ÀÌÅÍ : ");
-//	for (int i = 0; i < 10; i++) {
-//		scanf_s("%d", &arr[i]);
-//	}
-//
-//	printf("°Ë»ö µ¥ÀÌÅÍ ÀÔ·Â : ");
-//	scanf_s("%d", &search);
-//
-//	key_idx = binarySearch(arr, 0, sizeof(arr)/sizeof(int), search);
-//
-//	if (key_idx != -1)
-//		printf("À§Ä¡ : %d\n", key_idx);
-//	else
-//		printf("¾ø´Ù°í!¤±!\n");
-//}
-//
-//int binarySearch(int A[], int first, int last, int key)
-//{
-//	if (first > last)
-//		return -1;
-//
-//	int mid = (first + last) / 2;
-//
-//	if (key == A[mid]) 
-//		return mid;
-//	else if (key < A[mid])	
-//		binarySearch(A, first, mid-1, key);
-//	else 
-//		binarySearch(A, mid+1, last, key);
-//}
+#include <stdio.h>
+
+int binarySearch(int A[], int first, int last, int key);
+
+int main()
+{
+	int arr[10] = { 0 };
+	int search = 0;
+	int key_idx = 0;
+
+	printf("ì›ì‹œ ë°ì´í„° : ");
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	printf("ê²€ìƒ‰ ë°ì´í„° ì…ë ¥ : ");
+	scanf_s("%d", &search);
+
+	key_idx = binarySearch(arr, 0, sizeof(arr)/sizeof(int), search);
+
+	if (key_idx != -1)
+		printf("ìœ„ì¹˜ : %d\n", key_idx);
+	else
+		printf("ì—†ë‹¤ê³ !ã…!\n");
+}
+
+int binarySearch(int A[], int first, int last, int key)
+{
+	if (first > last)
+		return -1;
+
+	int mid = (first + last) / 2;
+
+	if (key == A[mid]) 
+		return mid;
+	else if (key < A[mid])	
+		binarySearch(A, first, mid-1, key);
+	else 
+		binarySearch(A, mid+1, last, key);
+}
