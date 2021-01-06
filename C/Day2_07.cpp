c67e160d8d756df30279014b0b4afd636257cae4
BitnaKeum <beausty23@naver.com>
2021-01-06 18:19:05 +0900

    겨울SW학교 2일차

--- C/Day2_07.cpp
+++ C/Day2_07.cpp
@@ -1,26 +1,26 @@
-//#include <stdio.h>
-//#include <string.h>
-//#define arrsize 1024
-//
-//void revPrint(const char* pStr);
-//
-//int main()
-//{
-//	char str[arrsize] = { 0 };
-//	gets_s(str, arrsize);
-//
-//	revPrint(str);
-//	
-//}
-//
-//void revPrint(const char* pStr) {
-//	const char* p1 = pStr;
-//	while (*p1)
-//		p1++;
-//	p1--;
-//
-//	while (p1 >= pStr) {
-//		putchar(*p1);
-//		p1--;
-//	}
-//}
+#include <stdio.h>
+#include <string.h>
+#define arrsize 1024
+
+void revPrint(const char* pStr);
+
+int main()
+{
+	char str[arrsize] = { 0 };
+	gets_s(str, arrsize);
+
+	revPrint(str);
+	
+}
+
+void revPrint(const char* pStr) {
+	const char* p1 = pStr;
+	while (*p1)
+		p1++;
+	p1--;
+
+	while (p1 >= pStr) {
+		putchar(*p1);
+		p1--;
+	}
+}
