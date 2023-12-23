5ac60b614c296ec00ee60f05f6240e47efb6bc27
BitnaKeum <beausty23@naver.com>
2023-12-23 15:56:35 +0900

    DFS 이용

--- 문제풀이/섬의 개수.py
+++ 문제풀이/섬의 개수.py
@@ -1,13 +1,14 @@
 # 책 파이썬 알고리즘 인터뷰 p.331
 # https://leetcode.com/problems/number-of-islands/
-# DFS 이용. 음료수 얼려먹기 문제랑 동일
+# DFS 이용. 음료수 얼려먹기 문제랑 거의 동일
 
+# --- 답안 1 ---
 def dfs(row, col):
     if row < 0 or row >= 4 or col < 0 or col >= 5:
-        return
+        return 0
     if grid_map[row][col] == 0:
-        return
-
+        return 0
+    
     global grid_map
     grid_map[row][col] = 0
     
@@ -15,7 +16,7 @@ def dfs(row, col):
     dfs(row+1, col)
     dfs(row, col-1)
     dfs(row, col+1)
-    
+    return 1
     
 answer = 0
 grid_map = []
@@ -24,8 +25,34 @@ for _ in range(4):
 
 for i in range(4):
     for j in range(5):
-        if grid_map[i][j] == 1:
-            dfs(i, j)
-            answer += 1
-
+        answer += dfs(i, j)
 print(answer)
+
+
+# --- 답안 2 ---
+# 조금 더 비효율적
+# def dfs(row, col):
+#     if row < 0 or row >= 4 or col < 0 or col >= 5:
+#         return
+#     if grid_map[row][col] == 0:
+#         return
+
+#     global grid_map
+#     grid_map[row][col] = 0
+    
+#     dfs(row-1, col)
+#     dfs(row+1, col)
+#     dfs(row, col-1)
+#     dfs(row, col+1)
+    
+# answer = 0
+# grid_map = []
+# for _ in range(4):
+#     grid_map.append(list(map(int, input())))
+
+# for i in range(4):
+#     for j in range(5):
+#         if grid_map[i][j] == 1:
+#             dfs(i, j)
+#             answer += 1
+# print(answer)
